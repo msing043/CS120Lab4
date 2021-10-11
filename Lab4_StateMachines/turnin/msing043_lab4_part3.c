b582ba95af02b1131f5fd4ab05b36a2b5ea36e21
msing043 <58718116+msing043@users.noreply.github.com>
2021-10-10 22:49:36 -0700

    Update msing043_lab4_part3.c

--- Lab4_StateMachines/turnin/msing043_lab4_part3.c
+++ Lab4_StateMachines/turnin/msing043_lab4_part3.c
@@ -2,82 +2,91 @@
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
-enum States {Start,Initial,PressPound,PressY, Unlock, Lock}SM;
-void timerTick(){
-	switch(SM) {
-		case Start:
-			SM=Initial;
-			break;
+enum Statemachine{Begin,Initializer,PressPound,YPressed, unlockDoor, lockDoor}SM;
+void LockMachine(){
+switch(SM) {
+	case Begin:
+		SM=Initializer;
+		break;
 
-		case Initial:
-			if((PINA & 0x07)==0x04) {
-				SM=PressPound;
-			}
-			else if ((PINA & 0x87)==0x80) {
-				SM=Lock;
-			}
-			else {
-				SM=Initial;
-			}
-			break;
-
-		case PressPound:
-			if((PINA & 0x07)==0x04) {
-                                SM=PressPound;
-                        }
-                        else {
-                                SM=Unlock;
-                        }
-                        break;
-	     case PressY:
-                        if ((PINA & 0x07)==0x02) {
-                                SM=Unlock;
-                        }
-                        else if(PINA ==0x00) {
-                                SM=Unlock;
-                        }
-                        else {
-                                SM=Initial;
-                        }
-                        break;
-		case Unlock:
-                        if ((PINA & 0x07)==0x02) {
-                                SM=PressY;
-                        }
-                        else {
-                                SM=Initial;
-                        }
-                        break;
-		case Lock:
-			if ((PINA & 0x87) == 0x80) {
-                                SM=Lock;
-                        }
-                        else{
-                                SM=Initial;
-                        }
-                        break;
+	case Initializer:
+		if((PINA&0x07)==0x04) {
+			SM=PressPound;
+		}
+		else if ((PINA&0x87)==0x80) {
+			SM=lockDoor;
+		}
+		else {
+			SM=Initializer;
+		}
+		break;
+		
+	case PressPound:
+		if((PINA&0x07)==0x04) {
+                	SM=PressPound;
+                }
+                else {
+                         SM=unlockDoor;
+                }
+                break;
+	case YPressed:
+		if ((PINA&0x07)==0x02) {
+                         SM=unlockDoor;
+                }
+                else if(PINA==0x00) {
+                         SM=unlockDoor;
+		}
+                else {
+                         SM=Initializer;
+                }
+		break;
+		
+	case unlockDoor:
+                if ((PINA&0x07)==0x02) {
+                         SM=YPressed;
+                }
+                else {
+                         SM=Initializer;
+                }
+                break;
+	case lockDoor:
+		if ((PINA&0x87)==0x80) {
+                          SM=lockDoor;
+                }
+                else{
+                          SM=Initializer;
+                }
+                break;
+	default:
+		SM=Begin;
+		break;
 	}
 	switch(SM) {
-		case Start:
-			PORTB=0x00;
-			break;
+	case Begin:
+		PORTB=0x00;
+		break;
 
-		case Initial:
-			break;
+	case Initializer:
+		break;
 
-		case PressPound:
-			break;
+	case PressPound:
+		break;
 
-		case PressY:
-			break;
+	case YPressed:
+		break;
 
-		case Unlock:
-			PORTB=0x01;
-			break;
+	case unlockDoor:
+		PORTB=0x01;
+		break;
 
-		case Lock:
-			PORTB=0x00;
-			break;
+	case lockDoor:
+		PORTB=0x00;
+		break;
+			
+	default:
+		PORTB=0x00;
+		break;
+		
 
 	}
 }
@@ -86,7 +95,7 @@ int main(void) {
     DDRA = 0x00; PORTA = 0xFF;
     DDRC = 0xFF; PORTC = 0x00;
     while (1) {
-	timerTick();
+	LockMachine();
    	 }
          return 1;
 	 }
